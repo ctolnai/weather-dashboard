378c34031098c1f1d5f0736998ebaeb1d27cce8f
Chris Tolnai <christolnai@gmail.com>
2021-08-24 19:22:47 -0700

    added logic for getting local storage and pulling API's

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ## https://github.com/ctolnai/weather-dashboard
 
 
-User enters a city into the search
+User enters a city into the search 
+    - save city into local storage
     - take city and enter into API url
     - load data into current forecast div
     - load data into 5 day forecast
     - use moment to add date into 5 day forecast
-    - save city into local storage
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,36 @@ var city = $("#citySearch");
 var city1 = $("#city1");
 
 // Save city into local storage:
-searchBtn.click(() => {   
-    localStorage.setItem("city" , city.val());
+searchBtn.click((event) => { 
+    event.preventDefault();
+    localStorage.setItem(city.val() , city.val())
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.val()}&units=imperial&appid=6a6da9ba64b6560cd35736d7ad6021e7`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            $("#city").text(` City: ${data.city.name}`)
+            $("#temp").text(` Temperature: ${data.list[0].main.temp} F`)
+            $("#wind").text(` Wind Speed: ${data.list[0].wind.speed} MPH`)
+            $("#humidity").text(` Humidity: ${data.list[0].main.humidity} %`)
+            console.log(data.list[0].main.temp)
+            console.log(data.city.name)
+
+
+            for (let i = 0; i <= 41; i+=7) {
+                console.log(i)
+                $(`#${i}`).text(data.list[i].main.temp)
+            }
+
+        })
 })
 
-
 // load city from local storage and put in recent cities card
-var userCity = localStorage.getItem("city")
+var userCity = localStorage.getItem(city)
+
 
 
 console.log(userCity)
 
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q=pasadena&appid=6a6da9ba64b6560cd35736d7ad6021e7")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-    })

de17dc48481090a14e977efef2ba200e3c52a688
Chris Tolnai <christolnai@gmail.com>
2021-08-25 15:02:00 -0700

    added uvindex

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ var city = $("#citySearch");
 var city1 = $("#city1");
 
 // Save city into local storage:
-searchBtn.click((event) => { 
+searchBtn.click((event) => {
     event.preventDefault();
-    localStorage.setItem(city.val() , city.val())
+    localStorage.setItem(city.val(), city.val())
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.val()}&units=imperial&appid=6a6da9ba64b6560cd35736d7ad6021e7`)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data)
             $("#city").text(` City: ${data.city.name}`)
             $("#temp").text(` Temperature: ${data.list[0].main.temp} F`)
@@ -19,12 +19,23 @@ searchBtn.click((event) => {
             $("#humidity").text(` Humidity: ${data.list[0].main.humidity} %`)
             console.log(data.list[0].main.temp)
             console.log(data.city.name)
-
-
-            for (let i = 0; i <= 41; i+=7) {
+            console.log(data.city.coord.lat)
+            console.log(data.city.coord.lon)
+            for (let i = 0; i <= 41; i += 7) {
                 console.log(i)
                 $(`#${i}`).text(data.list[i].main.temp)
             }
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&appid=6a6da9ba64b6560cd35736d7ad6021e7`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                    $("#uvIndex").text(` UV Index: ${data.current.uvi}`)
+
+                })
+
+            
 
         })
 })
